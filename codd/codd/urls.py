25c1f82d6d0b1276dead09ba2889d8a81ee6523d
BulPelm <45230714+Denisoid20000@users.noreply.github.com>
2024-12-05 00:59:01 +0300

    Подключена бд, настроена админка для управления, созданы модели. Нужно: 1.Написать обработчик 2.Построить запросы 3.Написать функции для получение данных из бд и отправки на фронт

--- codd/codd/urls.py
+++ codd/codd/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from main import views
 urlpatterns = [
     path('', views.MainPage),
-]
+    path('admin/', admin.site.urls)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- codd/main/admin.py
+++ codd/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Users, Adminusers, AcceptedRequests, Notifications, Saveduserpath, Supportchatlogging, Userrequests
+admin.site.register(Users)
+admin.site.register(Adminusers)
+admin.site.register(AcceptedRequests)
+admin.site.register(Notifications)
+admin.site.register(Saveduserpath)
+admin.site.register(Supportchatlogging)
+admin.site.register(Userrequests)
